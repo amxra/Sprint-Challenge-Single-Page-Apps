a74d1395a801b6070ba3ad960775ba30a5f20220
amxra <amiradediran@gmail.com>
2019-09-20 10:53:06 +0100

    Complete CharacterCard component and build JSX structure in charactercard.js

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  const {character} = props
+
+  return (
+    <div>
+      <h3>{character.name}</h3>
+      <img src ={character.image}></img>
+      <p>Gender : {character.gender}</p>
+      <p>Origin : {character.origin.name}</p>
+      <p>Specie : {character.species}</p>
+      <p>Status : {character.status}</p>
+
+
+    </div>
+  )
 }
