a4848332c54e7d6003d547253c93605f567a81da
amxra <amiradediran@gmail.com>
2019-09-20 11:14:17 +0100

    Import Form and render

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
-import {Route} from "react-router-dom";
 import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
+import axios from "axios";
+import Form from "./components/Form";
 
 
 export default function App() {
-  const [characters, setCharacters] = useState([])
+
+  const [characters, setCharacters] = useState([]);
+
+  const search = (formValue, actions) => {
+    const rightCharacters = characters.filter( chr => {
+      if(chr.name.toLowerCase().includes(formValue.name)){
+        return chr
+      }
+    })
+    setCharacters(rightCharacters);
+    actions.resetForm();
+  }
+
+  const validationSchema = {a:'boy'};
+
   return (
     <main>
       <Header />
       <Route exact path='/' render={props => <WelcomePage {...props} />} />
-      <Route path='/characters' render={props => <CharacterList {...props} characters={characters} setCharacters ={setCharacters}/>} />
+      <Route path='/characters' 
+        render={props => <Form 
+        {...props} 
+        search={search} 
+        validationSchema={validationSchema} />}
+      />
+      <Route path='/characters' 
+        render={props => <CharacterList 
+        {...props} 
+        characters={characters} 
+        setCharacters={setCharacters} />}
+      />
     </main>
   );
 }
