a3afc8e6e72739a898630acfa078a417f8672135
amxra <amiradediran@gmail.com>
2019-09-20 10:58:57 +0100

    Impoer charactercard, create props, create axios request and complete JSX in characterList.js

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
+
+  const {characters, setCharacters} = props
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/character/')
+    .then(response =>{
+      setCharacters(response.data.results)
+    })
+    .catch(err =>{
+      console.log(err);
+    })
   }, []);
 
   return (
